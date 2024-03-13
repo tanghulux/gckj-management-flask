fb2d4c65050b51a2527a600cbece14ee7ef27882
tanghulux <tanghulux@163.com>
2024-03-13 09:42:32 +0800

    修改wsgi文件

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
 import sys
 
 #app's path
-sys.path.insert(0,"C:/Users/Administrator/gckj-management")
+sys.path.insert(0,"C:/Users/Administrator/gckj-management-flask")
 
-from mytest import app
+from app import app
 
 #Initialize WSGI app object
 application = app
